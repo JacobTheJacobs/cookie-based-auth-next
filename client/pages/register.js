8b472d9f60b5bd8393e36432adabadc34a9e0820
Master J <jake.yanov@gmail.com>
2022-07-22 01:06:44 +0300

    login logout implementetion

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,27 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { SyncOutlined } from "@ant-design/icons";
+import Link from "next/link";
+import { Context } from "../context";
+import { useRouter } from "next/router";
+import user from "../../server/models/user";
 
 const Register = () => {
-    const [name, setName] = useState("mama");
-    const [email, setEmail] = useState("mama@gmail.com");
+    const [name, setName] = useState("Ryan");
+    const [email, setEmail] = useState("ryan@gmail.com");
     const [password, setPassword] = useState("rrrrrr");
     const [loading, setLoading] = useState(false);
+
+    const {
+        state: { user },
+    } = useContext(Context);
+
+    const router = useRouter();
+
+    useEffect(() => {
+        if (user !== null) router.push("/");
+    }, [user]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         // console.table({ name, email, password });
         try {
             setLoading(true);
-            const { data } = await axios.post(
-                `${process.env.NEXT_PUBLIC_API}/register`,
-                {
-                    name,
-                    email,
-                    password,
-                }
-            );
+            const { data } = await axios.post(`/api/register`, {
+                name,
+                email,
+                password,
+            });
             // console.log("REGISTER RESPONSE", data);
             toast("Registration successful. Please login.");
             setLoading(false);
@@ -33,7 +44,7 @@ const Register = () => {
 
     return (
         <>
-            <h1 className="jumbotron text-center bg-primary square">הרשמה</h1>
+            <h1 className="jumbotron text-center bg-primary square">הירשמו</h1>
             <div className="center">
                 <div className="container  col-md-4 offset-md-4 pb-5">
                     <form onSubmit={handleSubmit}>
@@ -71,11 +82,17 @@ const Register = () => {
                                     !name || !email || !password || loading
                                 }
                             >
-                                {loading ? <SyncOutlined spin /> : "הירשמו"}
+                                {loading ? <SyncOutlined spin /> : "הרשמה"}
                             </button>
                         </div>
                     </form>
                 </div>
+                <p className="text-center p-3">
+                    כבר רשומים?{" "}
+                    <Link href="/login">
+                        <a>כניסה</a>
+                    </Link>
+                </p>
             </div>
         </>
     );
